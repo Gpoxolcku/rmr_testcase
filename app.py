9cac8e621774726b315438cd75d6258a3d000982
Stanislav Grokholskiy <stangrokh@gmail.com>
2020-08-28 17:35:27 +0300

    Some organization changes

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, request, redirect, render_template
+import cv2
 import numpy as np
 from werkzeug.utils import secure_filename
-import cv2
+from flask import Flask, request, redirect, render_template
+
+from process import determine_dominate_color
 
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024
 UPLOAD_FOLDER = "tmp_images"
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+ALLOWED_EXTENSIONS = {'png'}
 
 
 def initialize_app(**kwargs):
@@ -20,16 +22,16 @@ app = initialize_app(
 )
 
 
-def is_file_allowed(filename):
+def is_file_allowed(filename, allowed_extentions=['png']):
     filename = filename.lower()
     return '.' in filename and \
-        filename.split('.')[-1] in ALLOWED_EXTENSIONS
+        filename.split('.')[-1] in allowed_extentions
 
 
 def read_image_file(request):
     image_file = request.files.get('file', None)
     filename = secure_filename(getattr(image_file, "filename", ""))
-    if not(image_file and is_file_allowed(filename)):
+    if not(image_file and is_file_allowed(filename, ALLOWED_EXTENSIONS)):
         print("Bad input file")
         return None
     # image_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
@@ -37,84 +39,6 @@ def read_image_file(request):
     image_decoded = cv2.imdecode(image, -1)
     image_decoded = cv2.cvtColor(image_decoded, cv2.COLOR_BGR2RGB)
     return image_decoded
-
-
-# debug
-def _calculate_mode(image):
-    if len(image.shape) == 2:  # grayscale
-        vals, counts = np.unique(image, return_counts=True)
-        v = vals[np.argmax(counts)]
-        return v
-
-    num_channels = image.shape[-1]
-    channels = np.split(image.astype(np.uint8), num_channels, axis=-1)
-    merged_colors = np.zeros_like(channels[0], dtype=np.uint32)
-    for ch in channels:  # rgb
-        merged_colors = merged_colors << 8
-        merged_colors += ch
-
-    vals, counts = np.unique(merged_colors, return_counts=True)
-    asort = np.argsort(counts)
-    # print(counts[asort][::-1][:10])
-    vals_sorted = vals[asort][::-1][:10]
-
-    results = []
-    for v in vals_sorted:
-        result = []
-        for _ in range(num_channels):  # bgr
-            c = v & 0xFF
-            v = v >> 8
-            result.append(c)
-        results.append(result[::-1])
-    return np.array(results)
-
-# debug
-def _determine_dominate_color(image):
-    mode = calculate_mode(image)
-    res = ""
-    for value in mode:
-        if isinstance(value, np.uint8):
-            value_str = f"({value},{value},{value})"
-        else:
-            value_str = ','.join(str(x) for x in value[:3])  # [:3] so the transparent result is visible
-    # return f'<p style="color:rgb({value_str});">{value}</p>'
-        s = f'<p style="background-color:rgb({value_str});">{value}</p>'
-        res = res + s
-    # return f'<p>{value}</p>'
-    return res
-
-
-def calculate_mode(image):
-    if len(image.shape) == 2:  # grayscale
-        vals, counts = np.unique(image, return_counts=True)
-        v = vals[np.argmax(counts)]
-        return v
-
-    num_channels = image.shape[-1]
-    channels = np.split(image.astype(np.uint8), num_channels, axis=-1)
-    merged_colors = np.zeros_like(channels[0], dtype=np.uint32)
-    for ch in channels:  # rgb
-        merged_colors = merged_colors << 8
-        merged_colors += ch
-    vals, counts = np.unique(merged_colors, return_counts=True)
-    v = vals[np.argmax(counts)]
-    result = []
-    for _ in range(num_channels):
-        c = v & 0xFF
-        v = v >> 8
-        result.append(c)
-    return np.array(result[::-1])
-
-
-def determine_dominate_color(image):
-    mode = calculate_mode(image)
-    value = mode
-    # if isinstance(value, np.uint8):
-    #     value_str = f"({value},{value},{value})"
-    # else:
-    #     value_str = ','.join(str(x) for x in value[:3])  # [:3] so the transparent result is visible
-    # return f'<p style="background-color:rgb({value_str});">{value}</p>'
-    return value
 
 
 @app.route("/api/determine_dominate_color", methods=['POST'])
