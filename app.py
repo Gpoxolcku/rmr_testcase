bed1618f6b39ddc021db912db53eed25765d1eed
Stanislav Grokholskiy <stangrokh@gmail.com>
2020-08-28 10:46:00 +0300

    root->index redirection

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello Flask!"
+def app_root():
+    return redirect(url_for("index"), code=300)
+    
+@app.route("/index")
+def index():
+    # return render_template("index")
+    return "Index page"
 
 if __name__ == "__main__":
     app.run()
